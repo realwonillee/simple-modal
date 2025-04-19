c237b36ec45fbd7961941d12b9debaf82ae1af28
realwonillee <slimshot@naver.com>
2025-04-19 20:25:13 +0900

    document: 모달 사용법 README.md 추가

--- src/lib/modal/types.ts
+++
@@ -1,9 +0,0 @@
-export interface IModal {
-  isOpen: boolean;
-  modalId: string;
-  actions: {
-    open: () => void;
-    replace: (isReplaceAll?: boolean) => void;
-    close: (isCloseAll?: boolean) => void;
-  };
-}
